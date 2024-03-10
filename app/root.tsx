3dfca90e4a5ea98285796595c37b1b914d87ab7e
Vlady Veselinov <vladkodmc@gmail.com>
2024-03-10 18:57:33 +0000

    update source

--- app/root.tsx
+++ app/root.tsx
@@ -20,7 +20,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <ScrollRestoration />
         <Scripts />
 
-        <footer><a href="https://github.com/vladinator1000/remix-drizzle-d1" target='_blank'>See source code</a></footer>
+        <footer><a href="https://github.com/vladinator1000/remix-drizzle-d1" target='_blank'>Source code</a></footer>
       </body>
     </html>
   )
